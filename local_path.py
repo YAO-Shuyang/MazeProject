30dcf5cd1975d524002eb85c0b2411800f606bb6
Shuyang Yao <qzyaoshuyang3@pku.edu.cn>
2025-11-21 16:52:26 -0500

    fixed some bugs

--- local_path.py
+++ local_path.py
@@ -2,25 +2,25 @@ import os
 import pandas as pd
 import numpy as np
 
-figpath = r'E:\Data\FinalResults'
-figdata = r'E:\Data\FigData'
+figpath = r'D:\Data\FinalResults'
+figdata = r'D:\Data\FigData'
 
-DMatrixPath = r"E:\Anaconda\envs\maze\Lib\site-packages\mylib\dismat"# r"E:\Anaconda\envs\maze\Lib\site-packages\mylib\decoder_DMatrix.pkl"
-CellregDate = r"E:\Anaconda\envs\maze\Lib\site-packages\mylib\cellreg"
+DMatrixPath = r"D:\Software\Anaconda3\envs\maze\Lib\site-packages\mylib\dismat"# r"D:\Anaconda\envs\maze\Lib\site-packages\mylib\decoder_DMatrix.pkl"
+CellregDate = r"D:\Software\Anaconda3\envs\maze\Lib\site-packages\mylib\cellreg"
 
-CM_path = r'E:\Data\Cross_maze'
-DSP_path = r'E:\Data\Dsp_maze'
-RM_path = r'E:\Data\Reverse_maze'
-HP_path = r'E:\Data\Hairpin_maze'
+CM_path = r'D:\Data\Cross_maze'
+DSP_path = r'D:\Data\Dsp_maze'
+RM_path = r'D:\Data\Reverse_maze'
+HP_path = r'D:\Data\Hairpin_maze'
 
 f1 = pd.read_excel(os.path.join(CM_path,'cross_maze_paradigm.xlsx'), sheet_name = 'calcium')
 f1_behav = pd.read_excel(os.path.join(CM_path,'cross_maze_paradigm.xlsx'), sheet_name = 'behavior')
-f_pure_behav = pd.read_excel(os.path.join(CM_path,'behavior_only.xlsx'), sheet_name = 'behavior')
-f_pure_old = pd.read_excel(os.path.join(CM_path,'behavior_only.xlsx'), sheet_name = 'prev')
+f_pure_behav = pd.read_excel(os.path.join(CM_path, "behavior_only.xlsx"), sheet_name = 'behavior')
+#f_pure_old = pd.read_excel(os.path.join(CM_path,'behavior_only.xlsx'), sheet_name = 'prev')
 
 f2 = pd.read_excel(os.path.join(DSP_path,'dsp_maze_paradigm.xlsx'), sheet_name = 'calcium')
 f2_behav = pd.read_excel(os.path.join(DSP_path,'dsp_maze_paradigm.xlsx'), sheet_name = 'behavior')
-f2_ori = pd.read_excel(os.path.join(DSP_path,'dsp_maze_paradigm.xlsx'), sheet_name = 'calcium_separate')
+#f2_ori = pd.read_excel(os.path.join(DSP_path,'dsp_maze_paradigm.xlsx'), sheet_name = 'calcium_separate')
 
 f3 = pd.read_excel(os.path.join(RM_path,'Reverse_maze_paradigm.xlsx'), sheet_name = 'calcium')
 f3_behav = pd.read_excel(os.path.join(RM_path,'Reverse_maze_paradigm.xlsx'), sheet_name = 'behavior')
@@ -28,13 +28,13 @@ f3_behav = pd.read_excel(os.path.join(RM_path,'Reverse_maze_paradigm.xlsx'), she
 f4 = pd.read_excel(os.path.join(HP_path,'Hairpin_maze_paradigm.xlsx'), sheet_name = 'calcium')
 f4_behav = pd.read_excel(os.path.join(HP_path,'Hairpin_maze_paradigm.xlsx'), sheet_name = 'behavior')
 
-f_CellReg_env = pd.read_excel(r"E:\Data\cellregs.xlsx", sheet_name='env')
-f_CellReg_day = pd.read_excel(r"E:\Data\cellregs.xlsx", sheet_name='day')
-f_CellReg_reverse = pd.read_excel(r"E:\Data\cellregs.xlsx", sheet_name='reverse')
-f_CellReg_day_hairpin = pd.read_excel(r"E:\Data\cellregs.xlsx", sheet_name='day_hairpin')
-f_CellReg_modi = pd.read_excel(r"E:\Data\cellregs.xlsx", sheet_name='modi')
-f_CellReg_dsp = pd.read_excel(r"E:\Data\cellregs.xlsx", sheet_name='dsp')
+f_CellReg_env = pd.read_excel(r"D:\Data\cellregs.xlsx", sheet_name='env')
+f_CellReg_day = pd.read_excel(r"D:\Data\cellregs.xlsx", sheet_name='day')
+f_CellReg_reverse = pd.read_excel(r"D:\Data\cellregs.xlsx", sheet_name='reverse')
+f_CellReg_day_hairpin = pd.read_excel(r"D:\Data\cellregs.xlsx", sheet_name='day_hairpin')
+f_CellReg_modi = pd.read_excel(r"D:\Data\cellregs.xlsx", sheet_name='modi')
+f_CellReg_dsp = pd.read_excel(r"D:\Data\cellregs.xlsx", sheet_name='dsp')
 
-f_decode = pd.read_excel(r"E:\Data\Simulation_pc\cross_maze_decode.xlsx", sheet_name='Lap-wise')
-f_decode_shuffle = pd.read_excel(r"E:\Data\Simulation_pc\cross_maze_decode.xlsx", sheet_name='Shuffle')
+#f_decode = pd.read_excel(r"D:\Data\Simulation_pc\cross_maze_decode.xlsx", sheet_name='Lap-wise')
+#f_decode_shuffle = pd.read_excel(r"D:\Data\Simulation_pc\cross_maze_decode.xlsx", sheet_name='Shuffle')
 
